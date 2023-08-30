f5b3bc7c96f229c05647a0fc8486f862abb061d8
Frankline Momanyi <39469432+franklinemomanyi@users.noreply.github.com>
2023-08-30 16:39:12 +0300

    Update connect.php
    Hide passwords / API keys

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 
 $servername = "localhost";
 $username = "root";
-$password = "onyonka97";
+$password = "xxxxxxxxxxxxxx";
 $dbname = "Nurses";
 
 // Create connection
@@ -11,6 +11,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
-
+    
 ?>
